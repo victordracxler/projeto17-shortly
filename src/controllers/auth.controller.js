018ce2ba79f0f201c66db1876c00728ca5c05452
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-12-22 19:24:28 -0300

    feat: sigin route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { connection } from '../database/db.js';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 export async function signUp(req, res) {
 	const { name, email, password, confirmPassword } = req.body;
@@ -14,7 +15,7 @@ export async function signUp(req, res) {
 			`
         SELECT email
         FROM users
-        WHERE email = $1
+        WHERE email = $1;
         `,
 			[email]
 		);
@@ -29,7 +30,7 @@ export async function signUp(req, res) {
 		await connection.query(
 			`
         INSERT INTO users (name, email, "hashPassword")
-        VALUES ($1, $2, $3)
+        VALUES ($1, $2, $3);
         `,
 			[name, email, hashPass]
 		);
@@ -41,4 +42,67 @@ export async function signUp(req, res) {
 	}
 }
 
-export async function signIn(req, res) {}
+export async function signIn(req, res) {
+	const { email, password } = req.body;
+
+	try {
+		const user = await connection.query(
+			`
+        SELECT *
+        FROM users
+        WHERE email = $1;
+        `,
+			[email]
+		);
+
+		if (user.rows.length === 0) {
+			res.sendStatus(401);
+			return;
+		}
+
+		const passwordCompare = bcrypt.compareSync(
+			password,
+			user.rows[0].hashPassword
+		);
+
+		if (!passwordCompare) {
+			res.sendStatus(401);
+			return;
+		}
+
+		const sessionExists = await connection.query(
+			`
+        SELECT *
+        FROM sessions
+        WHERE "userId" = $1;
+        `,
+			[user.rows[0].id]
+		);
+
+		if (sessionExists.rows.length != 0) {
+			await connection.query(
+				`
+            DELETE
+            FROM sessions
+            WHERE "userId" = $1;
+            `,
+				[user.rows[0].id]
+			);
+		}
+
+		const newToken = uuid();
+
+		await connection.query(
+			`
+        INSERT INTO sessions ("userId", token)
+        VALUES ($1, $2);
+        `,
+			[user.rows[0].id, newToken]
+		);
+
+		res.status(200).send(newToken);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { signUp } from '../controllers/auth.controller.js';
+import { signIn, signUp } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post('/signup', signUp);
+
+router.post('/signin', signIn);
 
 export default router;
